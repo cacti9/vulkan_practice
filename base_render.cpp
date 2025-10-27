66cf73a036e8412f5326c09433792ac60f2a3e4f
cacti9 <thebest21c@naver.com>
2025-10-27 20:49:53 +0900

    included vma header

--- base_render.cpp
+++ base_render.cpp
@@ -20,6 +20,9 @@ import vulkan_hpp;
 #include <GLFW/glfw3.h>
 #include <glm/glm.hpp>
 
+#define VMA_IMPLEMENTATION
+#include "vk_mem_alloc.h"
+
 constexpr uint32_t WIDTH = 800;
 constexpr uint32_t HEIGHT = 600;
 constexpr int MAX_FRAMES_IN_FLIGHT = 2;
